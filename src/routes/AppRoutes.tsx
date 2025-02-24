d18c003cf9b13b20b435ebfa5263571302031e2b
maatbm <matheusbmoraes0@gmail.com>
2025-02-24 08:55:39 -0300

    V1.2.1 - otimização de formatação dos arquivos

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,13 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Home, CreateNewTask} from '../pages/index';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, CreateNewTask } from "../pages/index";
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/novaTarefa" element={<CreateNewTask />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/novaTarefa" element={<CreateNewTask />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
