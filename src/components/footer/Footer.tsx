7f5bda37d589b0173ec3f3ef418fbd8b6c86c841
maatbm <matheusbmoraes0@gmail.com>
2025-02-24 08:48:53 -0300

    V1.2 - criação do footer e otimização do header

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './footerStyle.css';
+
+export function Footer(){
+    return(
+        <footer>
+            <h2>Todos os direitos reservados a ©Copyright</h2>
+        </footer>
+    )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 import './homeStyle.css';
 import logo from '../../assets/home/headerLogo.png';
+import { Footer } from '../../components/footer/Footer'; 
 
 export function Home(){
 return(
+    <>
     <header>
         <h1> Lista de tarefas </h1>
         <Link to="/novaTarefa" id='createTaskButton'> Criar uma tarefa </Link>
         <img alt='Logo' src={logo}></img>
     </header>
+    <main></main>
+    <Footer/>
+    </>
 )
 }
