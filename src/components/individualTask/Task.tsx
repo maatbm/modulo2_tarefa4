3a314db74131494438f1ea4991df070f651fb064
maatbm <matheusbmoraes0@gmail.com>
2025-02-24 22:29:07 -0300

    V1.3 - adição de uma página específica para criar uma nova tarefa

--- src/components/individualTask/Task.tsx
+++ src/components/individualTask/Task.tsx
@@ -1,9 +1,15 @@
 import './taskStyle.css';
+import deleteLogo from '../../assets/createTask/delete.png';
 
 export function Task(){
     return(
         <div className="task">
             <h1>Task</h1>
+            <div className="delete&checkContainer">
+            <input type="checkbox" id="checkTask" />
+            <label htmlFor="checkTask">Tarefa concluída</label>
+            <button><img className="deleteIcon" src={deleteLogo} alt="Deletar tarefa"/></button>
+          </div>
         </div>
     )
 }
--- src/pages/createNewTask/CreateNewTask.tsx
+++ src/pages/createNewTask/CreateNewTask.tsx
@@ -1,8 +1,32 @@
-export function CreateNewTask() {
-    return (
-        <div>
-            <h1>Create New Task</h1>
-        </div>
-    )
+import "./createNewTaskStyle.css";
+import { Footer } from "../../components/index";
 
+export function CreateNewTask() {
+  return (
+    <>
+      <div className="formContainer">
+        <form method="POST" id="taskForm">
+          <h2>Nova tarefa</h2>
+          <div className="inputContainer">
+            <label htmlFor="taskTitle">Título</label>
+            <input type="text" id="taskTitle" name="taskTitle" autoComplete="off" required/>
+          </div>
+          <div className="inputContainer">
+            <label htmlFor="taskDescription">Descrição</label>
+            <textarea id="taskDescription" name="taskDescription" autoComplete="off" required></textarea>
+          </div>
+          <div className="dateSubmitContainer">
+            <div className="inputContainer">
+              <label htmlFor="taskDate" id="dateLabel">Data limite</label>
+              <input type="date" id="taskDate" name="taskDate" required />
+            </div>
+            <div className="buttonContainer">
+              <button type="submit" className="buttonSubmit">Cadastrar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </>
+  );
 }
