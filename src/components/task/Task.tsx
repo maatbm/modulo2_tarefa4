19463f1b316ee8c46cbbc51177e4533cb72e2dfe
maatbm <matheusbmoraes0@gmail.com>
2025-02-26 10:06:22 -0300

    otimização

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,41 +1,41 @@
-import './taskStyle.css';
-import deleteIcon from '../../assets/home/delete.png';
-import { useContext } from 'react';
-import { TaskContext } from '../../contexts/TaskContext';
+import "./taskStyle.css";
+import deleteIcon from "../../assets/home/delete.png";
+import { useContext } from "react";
+import { TaskContext } from "../../contexts/TaskContext";
 
 type TaskProps = {
-    id: number;
-    title: string;
-    description: string;
-    date: string;
-    completed: boolean;
-}
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+  completed: boolean;
+};
 
 export function Task(task: TaskProps) {
-    const { deleteTask, toggleTaskCompleted } = useContext(TaskContext);
+  const { deleteTask, toggleTaskCompleted } = useContext(TaskContext);
 
-    return(
-        <div className='taskContainer' key={task.id}>
-          <h2>{task.title}</h2>
-          <p>{task.description}</p>
-          <p className='dataDisplay'>Data final: {task.date}</p>
-          <div className='taskButtons'>
-            <div className='checkboxContainer'>
-              <input
-               type='checkbox' 
-               className='taskCheckbox' 
-               id={`taskCheckbox${task.id}`} 
-               checked={task.completed} 
-               onChange={() => toggleTaskCompleted(task)}
-               /> 
-              <label htmlFor={`taskCheckbox${task.id}`}>Concluída</label> 
-            </div>
-            <div className='deleteContainer'>
-              <button id='deleteButton' onClick={() => deleteTask(task)}>
-                <img src={deleteIcon} alt='Delete icon' />
-              </button>
-            </div>
-          </div>
+  return (
+    <div className="taskContainer" key={task.id}>
+      <h2>{task.title}</h2>
+      <p>{task.description}</p>
+      <p className="dataDisplay">Data final: {task.date}</p>
+      <div className="taskButtons">
+        <div className="checkboxContainer">
+          <input
+            type="checkbox"
+            className="taskCheckbox"
+            id={`taskCheckbox${task.id}`}
+            checked={task.completed}
+            onChange={() => toggleTaskCompleted(task)}
+          />
+          <label htmlFor={`taskCheckbox${task.id}`}>Concluída</label>
         </div>
-    )
+        <div className="deleteContainer">
+          <button id="deleteButton" onClick={() => deleteTask(task)}>
+            <img src={deleteIcon} alt="Delete icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
